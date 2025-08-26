f78cedf17580235912541b46fa266e662ec5becb
Markus Min <markus.min@gmail.com>
2025-08-26 14:31:39 -0700

    Upper Columbia, hatchery Stan files

---
+++ Stan/upper_columbia_hatchery/UCH_submit_chains.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# submit the four different jobs for four different chains
+
+sbatch job_UCH_chain1.slurm —mpi=pmix_v1
+sbatch job_UCH_chain2.slurm —mpi=pmix_v1
+sbatch job_UCH_chain3.slurm —mpi=pmix_v1
+sbatch job_UCH_chain4.slurm —mpi=pmix_v1
